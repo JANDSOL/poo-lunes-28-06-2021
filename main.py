132c14152d8f170b5de8be844ff94ce23bd4b26b
Juan Andrade <jpas.juan23@gmail.com>
2021-07-08 13:11:01 -0500

    Finish Task.

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ class Operation:
     def multiplication(self):
         return self.number1 * self.number2
 
-    def integer_division(self):
+    def division(self):
         if self.number2 != 0: return self.number1 / self.number2
         return 0
 
@@ -27,8 +27,40 @@ class Operation:
         return self.number1 ** self.number2
     
     def show(self):
-        print('Operando1={}. Operando2={}'.format(self.number1, self. number2))
+        print('Operando1={}. Operando2={}'.format(self.number1,\
+                                                  self. number2))
 
 
 if __name__ == '__main__':
-    pass
+    print('Menú Operaciones Matemáticas')
+    print('1) Suma\n2) Resta\n3) Multiplicación\n4) División\
+           \n5) Division Entera\n6) Residuo\n7) Exponente\n8) Salir')
+    option = '0'
+
+    while option != '8':
+        option = input('Elija una opción[1...8]: ')
+        if option in '1234567':
+            num1 = int(input('Ingrese el número 1: '))
+            num2 = int(input('Ingrese el número 2: '))
+        if option == '1':
+            operation = Operation(num1, num2)
+            print('Suma:', operation.sum())
+        elif option == '2':
+            operation = Operation(num1, num2)
+            print('Resta:', operation.subtraction())
+        elif option == '3':
+            operation = Operation(num1, num2)
+            print('Multiplicación:', operation.multiplication())
+        elif option == '4':
+            operation = Operation(num1, num2)
+            print('División:', operation.division())
+        elif option == '5':
+            operation = Operation(num1, num2)
+            print('Division Entera:', operation.integer_division())
+        elif option == '6':
+            operation = Operation(num1, num2)
+            print('Residuo:', operation.residue())
+        elif option == '7':
+            operation = Operation(num1, num2)
+            print('Exponente:', operation.exponent())
+    print('Gracias por su visita!!!')
