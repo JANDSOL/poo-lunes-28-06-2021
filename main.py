6b72b046141d2ec2563af092d087d78761e8710a
Juan Andrade <jpas.juan23@gmail.com>
2021-07-08 09:57:41 -0500

    Class and methods created.

---
+++ main.py
@@ -0,0 +1,28 @@
+class Operation:
+    def __init__(self):
+        pass
+
+    def sum(self):
+        pass
+
+    def subtraction(self):
+        pass
+
+    def multiplication(self):
+        pass
+
+    def integer_division(self):
+        pass
+
+    def residue(self):
+        pass
+
+    def exponent(self):
+        pass
+    
+    def show(self):
+        pass
+
+
+if __name__ == '__main__':
+    pass
